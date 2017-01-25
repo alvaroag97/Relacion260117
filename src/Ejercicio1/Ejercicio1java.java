459e0c3f9ee7a62e874f55b591d1a2ef2c690456
1dam.m <1dam.m@AULA509.cenecmalaga.net>
2017-01-25 13:05:46 +0100

    Programa completo

--- src/Ejercicio1/Ejercicio1java.java
+++ src/Ejercicio1/Ejercicio1java.java
@@ -6,23 +6,43 @@ public class Ejercicio1java {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		float x=0;
-		x= cambio(x);
-		System.out.println("son "+x+ " fahrenheit");
+		float x = 0;
+		x = cambio(x);
+		System.out.println("son " + x + "º fahrenheit");
 
 	}
-	public static float cambio(float i){
-		Scanner aux= new Scanner(System.in);
-		System.out.print("Introduzca grados a cambiar: ");
-		float C= aux.nextFloat();
+
+	public static float cambio(float i) {
+		float C = 0;
+		C = validacion(C);
+
 		float F;
-		F = 32 + ( 9 * C / 5);
+		F = 32 + (9 * C / 5);
 		return F;
+
+	}
+
+	public static float validacion(float v) {
+		String x = "";
+		boolean error = false;
+		float n = 0;
+		Scanner aux = new Scanner(System.in);
 		
-		
-		
-		
-		
+		do {
+			System.out.print("Introduzca grados a cambiar: ");
+			x = aux.nextLine();
+			try {
+				n = Integer.parseInt(x);
+				error = false;
+			} catch (Exception e) {
+				System.out.println("no has introducido un numero");
+				error = true;
+			}
+
+		} while (error);
+
+		return n;
+
 	}
 
 }
