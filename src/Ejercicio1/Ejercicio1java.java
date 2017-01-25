ad1632da25d5e03fa9bd1e10ecc1ba5f50174518
1dam.m <1dam.m@AULA509.cenecmalaga.net>
2017-01-25 12:49:37 +0100

    primera version ejercicio

--- src/Ejercicio1/Ejercicio1java.java
+++ src/Ejercicio1/Ejercicio1java.java
@@ -1,10 +1,28 @@
 package Ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1java {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		float x=0;
+		x= cambio(x);
+		System.out.println("son "+x+ " fahrenheit");
 
+	}
+	public static float cambio(float i){
+		Scanner aux= new Scanner(System.in);
+		System.out.print("Introduzca grados a cambiar: ");
+		float C= aux.nextFloat();
+		float F;
+		F = 32 + ( 9 * C / 5);
+		return F;
+		
+		
+		
+		
+		
 	}
 
 }
